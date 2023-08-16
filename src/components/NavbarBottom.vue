9172da41ce3096baf2a6a2ee49bfdd1a2882a3ad
matkarimov <matkarim1099@gmail.com>
2023-08-16 10:14:38 +0500

    added search input nav top

--- src/components/NavbarBottom.vue
+++ src/components/NavbarBottom.vue
@@ -13,27 +13,27 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="fixed z-50 w-full h-14 -translate-x-1/2 bg-white border-t border-gray-300 bottom-1 left-1/2">
+  <div class="fixed z-50 w-full h-14 -translate-x-1/2 bg-white bottom-1 left-1/2">
     <div class="grid h-full max-w-lg grid-cols-5 mx-auto md:max-w-3xl">
       <router-link to="/" class="inline-flex flex-col items-center justify-center px-5 text-teal-400 hover:text-teal-600">
-        <HomeIcon class="w-6 h-6"/>
+        <HomeIcon class="w-8 h-8"/>
       </router-link>
       <router-link to="#" class="inline-flex flex-col items-center justify-center px-5 text-teal-400 hover:text-teal-600">
-        <StadiumIcon class="w-6 h-6"/>
+        <StadiumIcon class="w-8 h-8"/>
       </router-link>
-      <div class="flex items-center justify-center">
+      <div class="relative flex items-center justify-center">
         <router-link to="#"
-                     class="inline-flex items-center justify-center w-8 h-8 font-medium bg-teal-500 rounded-full">
-          <CreateIcon class="w-6 h-6 text-white"/>
+                     class="absolute -top-4 inline-flex items-center justify-center w-14 h-14  md:w-16 md:h-16 font-medium bg-teal-500 rounded-full shadow-xl">
+          <CreateIcon class="w-10 h-10 text-white"/>
         </router-link>
       </div>
       <router-link to="#"
                    class="inline-flex flex-col items-center justify-center px-5 text-teal-400 hover:text-teal-600">
-        <CalendarIcon class="w-6 h-6"/>
+        <CalendarIcon class="w-8 h-8"/>
       </router-link>
       <router-link to="#"
               class="inline-flex flex-col items-center justify-center px-5 text-teal-400 hover:text-teal-600">
-        <ProfileIcon class="w-6 h-6"/>
+        <ProfileIcon class="w-8 h-8"/>
       </router-link>
     </div>
   </div>
