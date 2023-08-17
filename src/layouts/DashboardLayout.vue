36d83d0cf1778b32fde5d009b4ea2cb178ff77b5
matkarimov <matkarim1099@gmail.com>
2023-08-17 18:43:17 +0500

    added navbar bottom, todays matches, finished matches sections -2

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,13 +1,9 @@
 <script setup>
 import NavbarBottom from "@/components/NavbarBottom.vue";
-import Navbar2 from "@/components/Navbar2.vue";
-
-
 </script>
 <template>
   <router-view v-slot="{ Component }">
     <component :is="Component"/>
   </router-view>
   <NavbarBottom/>
-<!--  <Navbar2/>-->
 </template>
