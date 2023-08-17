46bed7cac9543d50c991b8ef51cdcd20c941e63c
Umid2720 <umidbekruzmetov309@gmail.com>
2023-08-17 12:12:51 +0500

    fit (Home Page): added ViewPlayer component, edited style general scroll and added age property to players

--- src/components/BestPlayers.vue
+++ src/components/BestPlayers.vue
@@ -8,26 +8,26 @@ import { useViewPlayerStore } from '../stores/viewPlayer.store'
 import ViewPlayer from "../views/ViewPlayer.vue";
 
 const bestPlayers = ref([
-    { name: 'arslon', goal: 10, assist: 5, status: 'daily' },
-    { name: 'abror', goal: 8, assist: 8, status: 'weekly' },
-    { name: 'akmal', goal: 7, assist: 5, status: 'monthly' },
-    { name: 'bobur', goal: 5, assist: 5, status: 'daily' },
-    { name: 'elyor', goal: 5, assist: 5, status: 'daily' },
-    { name: 'qodirbergan', goal: 4, assist: 5, status: 'weekly' },
-    { name: 'ibrohim', goal: 4, assist: 5, status: 'monthly' },
-    { name: 'jamshid', goal: 4, assist: 5, status: 'monthly' },
-    { name: 'otabek', goal: 6, assist: 5, status: 'weekly' },
-    { name: 'ulugbek', goal: 6, assist: 5, status: 'weekly' },
-    { name: 'sardor', goal: 3, assist: 4, status: 'weekly' },
-    { name: 'shaxzod', goal: 2, assist: 3, status: 'monthly' },
-    { name: 'umid2', goal: 2, assist: 3, status: 'monthly' },
-    { name: 'doniyor', goal: 1, assist: 1, status: 'weekly' },
-    { name: 'yunusbek', goal: 3, assist: 1, status: 'daily' },
-    { name: 'behruz', goal: 3, assist: 0, status: 'weekly' },
-    { name: 'bekzod', goal: 1, assist: 1, status: 'daily' },
-    { name: 'umid', goal: 1, assist: 3, status: 'daily' },
-    { name: 'umidbek', goal: 1, assist: 3, status: 'daily' },
-    { name: 'elyor junior', goal: 1, assist: 1, status: 'monthly' }
+    { name: 'arslon', goal: 10, assist: 5, age: 27, status: 'daily' },
+    { name: 'abror', goal: 8, assist: 8, age: 20, status: 'weekly' },
+    { name: 'akmal', goal: 7, assist: 5, age: 25, status: 'monthly' },
+    { name: 'bobur', goal: 5, assist: 5, age: 27, status: 'daily' },
+    { name: 'elyor', goal: 5, assist: 5, age: 30, status: 'daily' },
+    { name: 'qodirbergan', goal: 4, assist: 5, age: 25, status: 'weekly' },
+    { name: 'ibrohim', goal: 4, assist: 5, age: 4, status: 'monthly' },
+    { name: 'jamshid', goal: 4, assist: 5, age: 28, status: 'monthly' },
+    { name: 'otabek', goal: 6, assist: 5, age: 28, status: 'weekly' },
+    { name: 'ulugbek', goal: 6, assist: 5, age: 27, status: 'weekly' },
+    { name: 'sardor', goal: 3, assist: 4, age: 18, status: 'weekly' },
+    { name: 'shaxzod', goal: 2, assist: 3, age: 22, status: 'monthly' },
+    { name: 'umid2', goal: 2, assist: 3, age: 28, status: 'monthly' },
+    { name: 'doniyor', goal: 1, assist: 1, age: 28, status: 'weekly' },
+    { name: 'yunusbek', goal: 3, assist: 1, age: 23, status: 'daily' },
+    { name: 'behruz', goal: 3, assist: 0, age: 29, status: 'weekly' },
+    { name: 'bekzod', goal: 1, assist: 1, age: 21, status: 'daily' },
+    { name: 'umid', goal: 1, assist: 3, age: 28, status: 'daily' },
+    { name: 'umidbek', goal: 1, assist: 3, age: 28, status: 'daily' },
+    { name: 'elyor junior', goal: 1, assist: 1, age: 16, status: 'monthly' }
 ])
 
 const best = ref('daily')
@@ -36,31 +36,33 @@ const reportBestPlayer = computed(() => bestPlayers.value.filter(player => playe
 </script>
 
 <template>
-    <section class="px-2">
-        <h2 class="text-3xl font-medium">Eng zo'r futbolchilar</h2>
-        <ul class="flex mt-2 mb-4 space-x-2">
-            <li @click="useViewPlayerStore().clearPlayer()">
-                <button class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
-                    :class="{ 'bg-teal-500 text-white': best == 'daily' }" @click="best = 'daily'">Kunlik</button>
-            </li>
-            <li @click="useViewPlayerStore().clearPlayer()">
-                <button class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
-                    :class="{ 'bg-teal-500 text-white': best == 'weekly' }" @click="best = 'weekly'">Haftalik</button>
-            </li>
-            <li @click="useViewPlayerStore().clearPlayer()">
-                <button class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
-                    :class="{ 'bg-teal-500 text-white': best == 'monthly' }" @click="best = 'monthly'">Oylik</button>
-            </li>
-        </ul>
+    <section class="px-2 max-h-96 overflow-y-auto">
+        <div class="sticky top-0 bg-gray-50 pb-4">
+            <h2 class="text-3xl font-medium">Eng zo'r futbolchilar</h2>
+            <ul class="flex mt-2 space-x-2">
+                <li @click="useViewPlayerStore().clearPlayer()">
+                    <button class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
+                        :class="{ 'bg-teal-500 text-white': best == 'daily' }" @click="best = 'daily'">Kunlik</button>
+                </li>
+                <li @click="useViewPlayerStore().clearPlayer()">
+                    <button class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
+                        :class="{ 'bg-teal-500 text-white': best == 'weekly' }" @click="best = 'weekly'">Haftalik</button>
+                </li>
+                <li @click="useViewPlayerStore().clearPlayer()">
+                    <button class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
+                        :class="{ 'bg-teal-500 text-white': best == 'monthly' }" @click="best = 'monthly'">Oylik</button>
+                </li>
+            </ul>
+        </div>
         <ul class="space-y-2">
             <li v-for="(player, index) in reportBestPlayer" :key="index" @click="useViewPlayerStore().setPlayer(player)"
                 class="flex items-center justify-between bg-white rounded-md p-2">
-                <button class="flex items-center gap-x-2" popovertarget="player">
+                <button class="flex items-center" popovertarget="player">
                     <PlaceOneIcon class="w-10 h-10" v-show="index == 0" />
                     <PlaceTwoIcon class="w-10 h-10" v-show="index == 1" />
                     <PlaceThreeIcon class="w-10 h-10" v-show="index == 2" />
                     <PlaceIcon class="w-10 h-10" v-show="index > 2" />
-                    <div class="bg-top bg-cover w-16 h-16 bg-no-repeat rounded-full"
+                    <div class="bg-top bg-cover w-16 h-16 bg-no-repeat rounded-full mx-2"
                         :style="{ backgroundImage: `url('${player.name}.jpg')` }"></div>
                     <div>
                         <h3 class="font-semibold capitalize">{{ player.name }}</h3>
--- src/views/ViewPlayer.vue
+++ src/views/ViewPlayer.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import CloseIcon from '../assets/icons/CloseIcon.vue';
 import { useViewPlayerStore } from '../stores/viewPlayer.store';
 
@@ -8,17 +8,55 @@ const playerInformation = computed(() => useViewPlayerStore().player)
 const clearAll = () => {
     useViewPlayerStore().clearPlayer()
 }
+
+const photos = ref(['arslon', 'abror', 'akmal', 'bobur', 'elyor', 'qodirbergan', 'ibrohim', 'jamshid', 'otabek', 'ulugbek', 'sardor', 'shaxzod', 'umid2', 'doniyor', 'yunusbek', 'behruz', 'bekzod', 'umid', 'umidbek', 'elyor junior'])
+
 </script>
 
 <template>
-    <section class="fixed w-full top-0 bg-white flex flex-col gap-y-5 p-10 border rounded z-10"
+    <section
+        class="fixed left-0 right-0 top-0 bg-white py-5 px-10 border rounded z-10 max-h-[340px] overflow-y-auto style-shadow"
         v-show="useViewPlayerStore().player.name">
-        <button class="absolute bg-[#16858D] rounded-full group right-4  top-10">
+        <button class="absolute bg-[#16858D] rounded-full group right-10 top-6 hover:bg-red-500">
             <CloseIcon class="w-10 h-10 text-white font-bold group-hover:rotate-90 duration-300" @click="clearAll()" />
         </button>
-        <div class="bg-top bg-cover w-20 h-20 bg-no-repeat rounded-full"
-            :style="{ backgroundImage: `url('${playerInformation.name}.jpg')` }"></div>
-        <h2 class="capitalize font-semibold text-3xl">{{ playerInformation.name }}</h2>
-        <h5>{{ playerInformation.age }}</h5>
+
+        <div class="flex items-center">
+            <div class="bg-top bg-cover w-20 h-20 mr-2 bg-no-repeat rounded-full"
+                :style="{ backgroundImage: `url('${playerInformation.name}.jpg')` }"></div>
+            <span class="-space-y-1">
+                <h2 class="capitalize font-semibold text-xl">{{ playerInformation.name }}</h2>
+                <h4>{{ playerInformation.name }}ov</h4>
+                <h4 class="text-[12px] text-gray-400">{{ playerInformation.age }} yosh</h4>
+            </span>
+        </div>
+        <ul class="flex my-4 items-center justify-around">
+            <li>
+                <h4 class="font-bold">1 000</h4>
+                <span>videolar</span>
+            </li>
+            <li>
+                <h4 class="font-bold">2 347</h4>
+                <span>obunachi</span>
+            </li>
+            <li>
+                <h4 class="font-bold">100</h4>
+                <span>gollar</span>
+            </li>
+        </ul>
+        <ul class="flex items-center flex-wrap gap-1">
+            <li class="bg-top bg-cover w-[32%] h-40 xl:h-96 xl:w-[30%] bg-no-repeat"
+                :style="{ backgroundImage: `url('${playerInformation.name}.jpg')` }">
+            </li>
+            <li v-for="(photo, index) in photos" :key="index"
+                class="bg-top bg-cover w-[32%] h-40 xl:h-96 xl:w-[30%] bg-no-repeat"
+                :style="{ backgroundImage: `url('${photo}.jpg')` }">
+            </li>
+        </ul>
     </section>
 </template>
+<style scoped>
+.style-shadow {
+    box-shadow: 0px -49px 36px -50px #18a9d1 inset;
+}
+</style>
