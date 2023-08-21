d7f88167c71d33f3bdd90a50cd98aaa7f6d276b0
Muhammad <mmadaminov020@gmail.com>
2023-08-21 14:00:15 +0500

    add stadium list 1

--- src/components/Home Sections/StadiumsListSection.vue
+++ src/components/Home Sections/StadiumsListSection.vue
@@ -43,7 +43,7 @@ const stadiumList = ref([
     id:1,
     listImage:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbNqFPWhGxt19V2DsmJPXhi2wtBrwYXEbAT3Wi1xvmab0uSrCG_7K43IY-7cOuo3pFlkU&usqp=CAU',
     assessment:"4.5",
-    title:'Brooklyn Pickle Court',
+    title:'Milliy Gazon',
     Item:'Dedicated * Outdoor',
     Mentor:"Batafsil..."
   },
@@ -51,7 +51,7 @@ const stadiumList = ref([
     id:2,
     listImage:'https://www.austadiums.com/news/images/tottenham-hotspur-stadium-football1.jpg',
     assessment:"4.9",
-    title:'Tottenham Hotspur ',
+    title:'Xorazm Palace',
     Item:'Hybrid * Indoor',
     Mentor:"Batafsil..."
   },
@@ -59,7 +59,7 @@ const stadiumList = ref([
     id:3,
     listImage:'https://i.guim.co.uk/img/media/936ffea3d696c9c42ad1741fc755542c0f1eb576/0_0_5700_3420/master/5700.jpg?width=1200&quality=85&auto=format&fit=max&s=f104a75e0cf7a96baa06e02f23003e24',
     assessment:"5.0",
-    title:'Brooklyn Pickle Court',
+    title:'Milliy Gazon',
     Item:'Dedicated * Outdoor',
     Mentor:"Batafsil..."
   },
@@ -67,7 +67,7 @@ const stadiumList = ref([
     id:3,
     listImage:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-Vpv-doYPXFLoYXvo9lO1pCxzLfGDyvr386CRb6PCEUCiL7nS6UF_OVCS5RaKWEsPuzA&usqp=CAU',
     assessment:"4.7",
-    title:'Tottenham Hotspur ',
+    title:'Xorazm Palace',
     Item:'Hybrid * Indoor',
     Mentor:"Batafsil..."
   },
@@ -75,7 +75,7 @@ const stadiumList = ref([
     id:3,
     listImage:'https://www.the8log.com/wp-content/uploads/2019/12/Football-Stadiums.png',
     assessment:"4.4",
-    title:'Brooklyn Pickle Court',
+    title:'Milliy Gazon',
     Item:'Dedicated * Outdoor',
     Mentor:"Batafsil..."
   },
@@ -83,7 +83,7 @@ const stadiumList = ref([
     id:3,
     listImage:'https://ficazfootball.com/wp-content/uploads/2021/03/West-Ham-United-F.C.-London-Stadium-1.jpg',
     assessment:"4.9",
-    title:'Tottenham Hotspur ',
+    title:'Xorazm Palace',
     Item:'Hybrid * Indoor',
     Mentor:"Batafsil..."
   },
@@ -91,7 +91,7 @@ const stadiumList = ref([
     id:3,
     listImage:'https://www.re-thinkingthefuture.com/wp-content/uploads/2021/02/A3309-Image-4_Allianz-Arena-Munich-Germany_%C2%A9allianz-arena.com_.jpg',
     assessment:"5.0",
-    title:'Brooklyn Pickle Court',
+    title:'Milliy Gazon',
     Item:'Dedicated * Outdoor',
     Mentor:"Batafsil..."
   },
@@ -99,7 +99,7 @@ const stadiumList = ref([
     id:3,
     listImage:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvwJ_s8CbcfO9FMA1raB6Ay76ljPOA4GJeykhxEweTalHc7JXTkecFlH--pwTC-w8gJT8&usqp=CAU',
     assessment:"4.6",
-    title:'Tottenham Hotspur ',
+    title:'Xorazm Palace',
     Item:'Hybrid * Indoor',
     Mentor:"Batafsil..."
   },
@@ -107,7 +107,7 @@ const stadiumList = ref([
     id:3,
     listImage:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHcYeJ6UYeqJLiwjWihQ2aPEekNUVuQnvBkeUT0F3kOqvRVj_l9wcVjjFX_bOvxunTems&usqp=CAU',
     assessment:"4.3",
-    title:'Brooklyn Pickle Court',
+    title:'Milliy Gazon',
     Item:'Dedicated * Outdoor',
     Mentor:"Batafsil..."
   },
@@ -117,12 +117,12 @@ const stadiumList = ref([
 
 </script>
 <template>
-  <div class="relative mx-auto max-w-lg pb-1 md:max-w-lg text-center">
+  <div class="relative mx-auto max-w-lg mb-8 pb-1 md:max-w-lg text-center">
     <div>
-      <div class="flex text-center text items-center space-x-16 justify-around md:justify-between">
-        <h1 class="md:text-2xl text-xl text-zinc-800 font-semibold">Popular Stadiums</h1>
-          <span class="md:text-lg text-sm md:font-medium font-semibold text-blue-600">See all</span>
-      </div>
+        <div class="flex justify-between max-w-lg items-center md:mx-auto text-teal-600 font-bold px-2">
+          <h1 class="text-xl">Stadionlar ro'yhati</h1>
+          <a href="" class="text-xs">Barchasi</a>
+        </div>
       <swiper
           :slidesPerView="slidesPerView"
           :centeredSlides="true"
@@ -159,7 +159,7 @@ const stadiumList = ref([
                   <p class="mb-4 mt-2 text-zinc-500 text-sm font-normal dark:text-zinc-400">{{list.Item}}</p>
                 </div>
                 <div class="flex justify-between items-center mt-2">
-                  <button class="bg-cyan-600 hover:bg-cyan-700 transition duration-200 text-sm text-gray-50  rounded-full p- py-1.5 px-4">
+                  <button class="bg-teal-400 hover:bg-teal-500 transition duration-200 text-sm font-semibold text-gray-50  rounded-full p- py-1.5 px-4">
                       Booking
                   </button>
                   <RouterLink to="/stadiums" class="text-md mt-1 font-normal border-l-2 pl-6 text-zinc-500">
