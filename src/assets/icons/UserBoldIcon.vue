89b0ffe4b9de2091def0a10c0e002f35efb23a35
matkarimov <matkarim1099@gmail.com>
2023-08-23 16:39:38 +0500

    FIT (added EditProfile page)

--- src/assets/icons/UserBoldIcon.vue
+++ src/assets/icons/UserBoldIcon.vue
@@ -1,4 +1,4 @@
-    <template>
+<template>
   <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
     <g fill="currentColor">
       <circle cx="12" cy="6" r="4"></circle>
