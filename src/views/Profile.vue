24a65edc349ed64c1df56f5d74cb281951fdffd8
Umid2720 <umidbekruzmetov309@gmail.com>
2023-08-23 16:01:16 +0500

    added profile section

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,11 +1,20 @@
 <script setup>
-
 </script>
 
 <template>
-<h1 class="text-2xl font-bold px-5">Assalomu alaykum Siz Profile.vue dasiz.</h1>
+  <section class="overflow-hidden max-w-lg mx-auto">
+    <div class="bg-teal-400 h-48 md:h-56"></div>
+    <div class="relative bg-gray-50 w-96 h-96 leaf mx-auto -top-36 md:-top-44">
+      <div
+        class="bg-top bg-cover w-20 h-20 bg-no-repeat rounded-full bg-[url('/public/arslon.jpg)']"
+      ></div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
-
+.leaf {
+  border-radius: 0 100%;
+  transform: rotate(315deg);
+}
 </style>
