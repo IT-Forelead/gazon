394700daa75f2e3d217d6c8bdb4c5786cbda48d7
Umid2720 <umidbekruzmetov309@gmail.com>
2023-08-29 16:40:35 +0500

    fixed (navbar bottom): fixed navbar bottom styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script setup>
-import { computed } from 'vue'
-import { useRouter } from 'vue-router'
-import NavbarTop from "@/components/NavbarTop.vue";
+import { computed } from "vue";
+import { useRouter } from "vue-router";
+import NavbarBottom from "@/components/NavbarBottom.vue";
 
-const defaultLayout = 'login'
-const { currentRoute } = useRouter()
-const layout = computed(() => `${currentRoute.value.meta.layout || defaultLayout}-layout`)
+const defaultLayout = "login";
+const { currentRoute } = useRouter();
+const layout = computed(
+  () => `${currentRoute.value.meta.layout || defaultLayout}-layout`
+);
 </script>
 
 <template>
-    <component :is="layout">
-      <router-view />
-    </component>
+  <component :is="layout" class="pb-16">
+    <router-view />
+  </component>
+  <NavbarBottom />
 </template>
