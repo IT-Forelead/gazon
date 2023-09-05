d753177ae4395f2911b601a2636b6a6513bc26b8
Umid2720 <umidbekruzmetov309@gmail.com>
2023-09-05 11:05:31 +0500

    fix (stadiumList, stadiumlist.json): fixed responsive styles and stadium list.json dates

--- src/components/Home Sections/StadiumsListSection.vue
+++ src/components/Home Sections/StadiumsListSection.vue
@@ -1,47 +1,31 @@
 <script setup>
-import StarIcon from "@/assets/icons/StarIcon.vue";
-import {computed } from "vue";
 import "swiper/css";
-import { ref, computed, onMounted } from "vue";
 import "swiper/css/navigation";
 import { useRouter } from "vue-router";
+import { ref, computed, onMounted } from "vue";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import StarIcon from "@/assets/icons/StarIcon.vue";
 import { Autoplay, Pagination } from "swiper/modules";
-import stadiumList1 from "@/assets/data/stadiumList.json";
-
-
-const router = useRouter()
-
-const screenSize = computed(() => {
-  return window.innerWidth;
-
-})
+import stadiumList from "@/assets/data/stadiumList.json";
 
 const screenSize = ref(0);
+const router = useRouter();
 
 const slidesPerView = computed(() => {
   if (screenSize.value < 340) {
-    // iPhone X portrait width
     return 1.3;
-  }
-  else if (screenSize.value < 376) {
-    // Other mobile devices
+  } else if (screenSize.value < 377) {
     return 1.5;
-  }
-  else if (screenSize.value < 394) {
-    // Other mobile devices
-  if (screenSize.value < 390) {
-    return 1.4;
-  } else if (screenSize.value < 464) {
+  } else if (screenSize.value < 400) {
     return 1.6;
-  }
-  else if (screenSize.value < 768) {
-    // Other mobile devices
+  } else if (screenSize.value < 440) {
+    return 1.7;
+  } else if (screenSize.value < 464) {
     return 1.8;
-  } else {
-    // Desktop and larger screens
+  } else if (screenSize.value < 510) {
     return 2;
+  } else {
+    return 4;
   }
 });
 
@@ -54,15 +38,6 @@ const spaceBetween = computed(() => {
     return -27;
   } else if (screenSize.value < 568) {
     return -7;
-  if (screenSize.value < 375) {
-    // iPhone X portrait width
-    return -8;
-  } else if (screenSize.value < 768) {
-    //Other mobile device
-    return -10;
-  } else {
-    // Desktop and larger screens
-    return -30;
   }
 });
 
@@ -84,54 +59,77 @@ onMounted(() => {
 const modules = [Autoplay, Pagination];
 </script>
 <template>
-  <div class="relative mx-auto max-w-lg mb-8 pb-1 md:max-w-lg text-center">
+  <div class="relative mx-auto max-w-3xl mb-8 pb-1 text-center">
     <div>
-      <div class="flex justify-between max-w-lg items-center md:mx-auto text-teal-600 font-bold px-2">
+      <div
+        class="flex justify-between items-center md:mx-auto text-teal-600 font-bold px-2"
+      >
         <h1 class="text-xl">Stadionlar ro'yhati</h1>
         <a href="" class="text-xs">Barchasi</a>
       </div>
-      <swiper :slidesPerView="slidesPerView"
-              :centeredSlides="true"
-              :spaceBetween="spaceBetween"
-              :grabCursor="true"
-              :pagination="{ clickable: true, }"
-              :autoplay="{
-               delay: 3500,
-               disableOnInteraction: false,
-               }"
-              :loop="true"
-              :modules="modules" class="mySwiper">
-        <swiper-slide v-for="(list, index) in stadiumList1" :key="index"
-          @click="router.push(`/View-stadiums/${list.id}`)">
+      <swiper
+        :slidesPerView="slidesPerView"
+        :centeredSlides="true"
+        :spaceBetween="spaceBetween"
+        :grabCursor="true"
+        :pagination="{ clickable: true }"
+        :autoplay="{
+          delay: 3500,
+          disableOnInteraction: false,
+        }"
+        :loop="true"
+        :modules="modules"
+        class="mySwiper"
+      >
+        <swiper-slide
+          v-for="(list, index) in stadiumList"
+          :key="index"
+          @click="router.push(`/View-stadiums/${list.id}`)"
+        >
           <div class="overflow-hidden -mr-10 mt-6">
             <div class="flex transition-transform duration-500 ease-in-out">
               <div class="px-4">
                 <div
-                  class="bg-white border shadow-xl hover:shadow-xl w-56 transition duration-300 flex flex-col rounded-xl">
+                  class="bg-white border shadow-xl hover:shadow-xl w-56 transition duration-300 flex flex-col rounded-xl"
+                >
                   <div class="relative">
                     <div
-                      class="w-20 h-8 flex items-center absolute top-3 left-3 bg-gray-200 text-black text-lg rounded-3xl p-2">
+                      class="w-20 h-8 flex items-center absolute top-3 left-3 bg-gray-200 text-black text-lg rounded-3xl p-2"
+                    >
                       <StarIcon />
-                      <a class="text-lg font-semibold ml-2">{{ list.assessment }}</a>
+                      <a class="text-lg font-semibold ml-2">{{
+                        list.assessment
+                      }}</a>
                     </div>
-                    <div class="h-36 bg-cover bg-center rounded-t-lg" :style="{ backgroundImage: `url(/images/${list.listImage}.jpg)` }">
-                    </div>
+                    <div
+                      class="h-36 bg-cover bg-center rounded-t-lg"
+                      :style="{
+                        backgroundImage: `url(/images/${list.images[0]}.jpg)`,
+                      }"
+                    ></div>
                   </div>
                   <div class="py-2.5 px-2.5 flex flex-col">
-                    <div class="text-left  border-b-2">
-                      <a href="#" class="font-semibold  text-zinc-900">
+                    <div class="text-left border-b-2">
+                      <a href="#" class="font-semibold text-zinc-900">
                         {{ list.title }}
                       </a>
-                      <p class="mb-4 mt-1 text-zinc-500 text-sm font-normal dark:text-zinc-400">{{ list.Item }}</p>
+                      <p
+                        class="mb-4 mt-1 text-zinc-500 text-sm font-normal dark:text-zinc-400"
+                      >
+                        {{ list.address }}
+                      </p>
                     </div>
                     <div class="flex justify-between items-center mt-2">
                       <button
-                        class="bg-teal-400 hover:bg-teal-500 transition duration-200 text-sm font-Semibold text-gray-50  rounded-full p- py-1.5 px-4">
+                        class="bg-teal-400 hover:bg-teal-500 transition duration-200 text-sm font-Semibold text-gray-50 rounded-full p- py-1.5 px-4"
+                      >
                         Booking
                       </button>
-                      <div @click="router.push(`/View-stadiums/${list.id}`)"
-                        class="text-lg mt-1 font-normal border-l-2 pl-5 text-zinc-500">
-                     Batafsil...
+                      <div
+                        @click="router.push(`/View-stadiums/${list.id}`)"
+                        class="text-lg mt-1 font-normal border-l-2 pl-5 text-zinc-500"
+                      >
+                        Batafsil...
                       </div>
                     </div>
                   </div>
--- src/views/ViewStadium.vue
+++ src/views/ViewStadium.vue
@@ -1,83 +1,93 @@
 <script setup>
-import ArrowLeftIcon from "@/assets/icons/ArrowLeftIcon.vue";
+import { useRoute } from "vue-router";
+import { onMounted, onUpdated, ref } from "vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
 import CallIcon from "@/assets/icons/CallIcon.vue";
-import {onMounted, onUpdated, ref} from "vue";
 import StarIcon from "@/assets/icons/StarIcon.vue";
 import UserIcon from "@/assets/icons/UserIcon.vue";
-import {useRoute} from "vue-router";
-import stadiumList2 from '@/data/stadiumList.json'
 import FieldIcon from "@/assets/icons/FieldIcon.vue";
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import {Autoplay, Pagination, Navigation, EffectCube} from "swiper/modules";
-import StadiumsListSection from "@/components/Home Sections/StadiumsListSection.vue";
+import stadiumList from "@/assets/data/stadiumList.json";
 import LocationIcon from "@/assets/icons/LocationIcon.vue";
+import ArrowLeftIcon from "@/assets/icons/ArrowLeftIcon.vue";
+import { Autoplay, Pagination, Navigation, EffectCube } from "swiper/modules";
+import StadiumsListSection from "@/components/Home Sections/StadiumsListSection.vue";
 
+const stadion = ref();
+const stdions = ref([]);
+const route = useRoute();
+const modules = [Autoplay, Pagination, Navigation, EffectCube];
 
-
-
-const modules = [Autoplay, Pagination, Navigation,EffectCube];
-const hero = ref()
-const players = ref([])
-const route = useRoute()
-const booking = ref([''])
 onMounted(() => {
-  hero.value = stadiumList2.filter(player => player.id == route.params.id)[0]
-  players.value = stadiumList2.filter(player => player.id != route.params.id)
-  players.value.unshift(hero.value)
-})
+  stadion.value = stadiumList.filter((item) => item.id == route.params.id)[0];
+  stdions.value = stadiumList.filter((item) => item.id != route.params.id);
+  stdions.value.unshift(stadion.value);
+});
+
 onUpdated(() => {
-  hero.value = stadiumList2.filter(player => player.id == route.params.id)[0]
-  players.value = stadiumList2.filter(player => player.id != route.params.id)
-  players.value.unshift(hero.value)
-})
+  stadion.value = stadiumList.filter(
+    (player) => player.id == route.params.id
+  )[0];
+  stdions.value = stadiumList.filter((item) => item.id != route.params.id);
+  stdions.value.unshift(stadion.value);
+});
 </script>
 
 <template>
-  <div class="max-w-lg px-1 pb-1 md:max-w-lg    mx-auto md:mx-auto">
+  <div class="max-w-lg px-1 pb-1 md:max-w-lg mx-auto md:mx-auto">
     <div class="items-center mb-6">
-      <RouterLink to="/" class=""><ArrowLeftIcon/></RouterLink>
+      <RouterLink to="/" class=""><ArrowLeftIcon /></RouterLink>
       <h1 class="text-xl text-center font-bold text-teal-600">Batafsil</h1>
     </div>
     <swiper
-        :slidesPerView="1"
-        :spaceBetween="5"
-        :loop="true"
-        :pagination="{
-      clickable: true,
-    }"
-        :modules="modules"
-        class="mySwiper"
+      :slidesPerView="1"
+      :spaceBetween="5"
+      :loop="true"
+      :pagination="{
+        clickable: true,
+      }"
+      :modules="modules"
+      class="mySwiper"
     >
-      <swiper-slide v-for="(image, key) in hero?.listImage1" :key="key">
+      <swiper-slide v-for="(image, key) in stadion?.images" :key="key">
         <div class="w-full h-80">
-          <div class="w-full h-full mx-auto rounded-xl bg-cover bg-center" :style="{ backgroundImage: `url(/images/${image}.jpg)` }">
-          </div>
+          <div
+            class="w-full h-full mx-auto rounded-xl bg-cover bg-center"
+            :style="{ backgroundImage: `url(/images/${image}.jpg)` }"
+          ></div>
         </div>
       </swiper-slide>
     </swiper>
     <div class="flex justify-between items-center mt-4">
-      <h1 class="text-3xl  md:text-4xl font-bold">{{hero?.title}}</h1>
-      <button class="text-md text-teal-600 bg-teal-100 rounded-md hover:bg-teal-50 font-black ml-8 px-3 py-2">
+      <h1 class="text-3xl md:text-4xl font-bold">{{ stadion?.title }}</h1>
+      <button
+        class="text-md text-teal-600 bg-teal-100 rounded-md hover:bg-teal-50 font-black ml-8 px-3 py-2"
+      >
         Top Sales
       </button>
     </div>
     <div class="flex mt-2 space-x-2">
-      <span class="text-blue-700 font-bold"><LocationIcon/></span>
+      <span class="text-blue-700 font-bold"><LocationIcon /></span>
       <span class="text-blue-600 border-r pr-4">100 mi</span>
-      <p class="text-md  ">{{hero?.Item}} Xorazm Palace 24-son maktab yoni </p>
+      <p class="text-md">
+        {{ stadion?.Item }} Xorazm Palace 24-son maktab yoni
+      </p>
     </div>
-    <div class=" " >
-      <iframe class="rounded-2xl my-3.5 mx-auto h-56 w-4/5 "  src="https://yandex.ru/map-widget/v1/?um=constructor%3A0861d5d052d7a68acfd29d258e16d301bf0474c689e21cece960b0f2abbbaedc&amp;source=constructor"  frameborder="0"></iframe>
+    <div class=" ">
+      <iframe
+        class="rounded-2xl my-3.5 mx-auto h-56 w-4/5"
+        src="https://yandex.ru/map-widget/v1/?um=constructor%3A0861d5d052d7a68acfd29d258e16d301bf0474c689e21cece960b0f2abbbaedc&amp;source=constructor"
+        frameborder="0"
+      ></iframe>
     </div>
 
     <div class="flex justify-between items-center mt-2">
       <div class="flex items-center space-x-2">
-        <h1 class="text-3xl font-bold"><StarIcon/></h1>
-        <h1 class="text-lg font-semibold">{{hero?.assessment}}</h1>
-        <h1 class="text-lg">({{hero?.reviews}} Reviews)</h1>
+        <h1 class="text-3xl font-bold"><StarIcon /></h1>
+        <h1 class="text-lg font-semibold">{{ stadion?.assessment }}</h1>
+        <h1 class="text-lg">({{ stadion?.reviews }} Reviews)</h1>
       </div>
       <div class="flex items-center">
-        <h1 class="text-2xl md:text-3xl font-bold">{{hero?.price}}</h1>
+        <h1 class="text-2xl md:text-3xl font-bold">{{ stadion?.price }}</h1>
         <h1 class="text-md md:text-lg ml-1">so'm dan</h1>
       </div>
     </div>
@@ -85,41 +95,49 @@ onUpdated(() => {
       <span class="text-lg font-bold">Xususiyatlari:</span>
       <div class="flex justify-start space-x-6 items-center mt-1">
         <div class="flex space-x-1 items-center">
-          <h1 class="text-3xl font-bold"><UserIcon/></h1>
-          <h1 class="text-lg">{{hero?.reviews}}ta odam</h1>
+          <h1 class="text-3xl font-bold"><UserIcon /></h1>
+          <h1 class="text-lg">{{ stadion?.reviews }}ta odam</h1>
         </div>
         <div class="flex space-x-1 items-center">
-          <FieldIcon/>
-          <h1 class="text-lg">{{hero?.field}}</h1>
+          <FieldIcon />
+          <h1 class="text-lg">{{ stadion?.field }}</h1>
         </div>
       </div>
     </div>
     <div class="items-center mt-3">
       <span class="text-lg font-bold">Tasnifi:</span>
-      <p class="text-left ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+      <p class="text-left">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's
+      </p>
     </div>
     <div class="items-center mb-auto mt-2">
-    <span class="text-lg font-bold">Band qilish:</span>
+      <span class="text-lg font-bold">Band qilish:</span>
 
-    <div class="flex justify-around  mb-3 mt-2  text-center items-center">
-      <div class="flex">
-        <h1 class="text-lg rounded-l-lg border-2 px-6 py-1.5 bg-teal-50 border-teal-50  hover:bg-teal-50  transition duration-300">{{hero?.phone}}</h1>
-        <button class="border-2 px-6 py-2 bg-teal-200 border-teal-200 hover:bg-teal-100 transition duration-300 rounded-r-lg">
-          <a href="tel:+998942469111"><CallIcon/></a>
+      <div class="flex justify-around mb-3 mt-2 text-center items-center">
+        <div class="flex">
+          <h1
+            class="text-lg rounded-l-lg border-2 px-6 py-1.5 bg-teal-50 border-teal-50 hover:bg-teal-50 transition duration-300"
+          >
+            {{ stadion?.phone }}
+          </h1>
+          <button
+            class="border-2 px-6 py-2 bg-teal-200 border-teal-200 hover:bg-teal-100 transition duration-300 rounded-r-lg"
+          >
+            <a href="tel:+998942469111"><CallIcon /></a>
+          </button>
+        </div>
+
+        <button
+          class="border-2 px-6 py-2 bg-teal-200 border-teal-200 hover:bg-teal-100 transition duration-300 rounded-xl"
+        >
+          <label class="text-gray-300-50">Belgilash</label>
         </button>
       </div>
-
-
-      <button class="border-2 px-6 py-2 bg-teal-200 border-teal-200 hover:bg-teal-100 transition duration-300 rounded-xl">
-        <label class="text-gray-300-50">Belgilash</label>
-      </button>
-
     </div>
-  </div>
     <div class="mb-10">
-       <StadiumsListSection/>
+      <StadiumsListSection />
     </div>
-
   </div>
 </template>
 
