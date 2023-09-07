560082f24329d9fd6afc2d4f67724d7496803c34
Umid2720 <umidbekruzmetov309@gmail.com>
2023-09-07 16:02:32 +0500

    fix (Home page): fixed stadiumsList name

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 import BestPlayers from "@/views/BestPlayers.vue"
 import NavbarTop from "@/components/NavbarTop.vue";
 import SearchingSection from "@/components/Home Sections/SearchingSection.vue"
-import StadiumsListSection from "@/components/Home Sections/StadiumsListSection.vue"
+import StadiumsSection from "@/components/Home Sections/StadiumsSection.vue"
 import TodaysMatchesSection from "@/components/Home Sections/TodaysMatchesSection.vue"
 import FinishedMatchesSection from "@/components/Home Sections/FinishedMatchesSection.vue"
 </script>
@@ -11,7 +11,7 @@ import FinishedMatchesSection from "@/components/Home Sections/FinishedMatchesSe
   <div class="min-h-screen bg-gray-50">
     <NavbarTop />
     <SearchingSection />
-    <StadiumsListSection/>
+    <StadiumsSection/>
     <TodaysMatchesSection/>
     <BestPlayers />
     <FinishedMatchesSection/>
