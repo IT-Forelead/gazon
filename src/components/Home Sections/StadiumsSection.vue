7a2051d9bd25acc4eba9d81c5aa7392522691259
Umid2720 <umidbekruzmetov309@gmail.com>
2023-09-08 16:54:31 +0500

    fix (StadiumsSection): fixed full responsive stadiums card

--- src/components/Home Sections/StadiumsSection.vue
+++ src/components/Home Sections/StadiumsSection.vue
@@ -5,6 +5,7 @@ import { useRouter } from "vue-router";
 import { ref, computed, onMounted } from "vue";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import StarIcon from "@/assets/icons/StarIcon.vue";
+import SortButton from "../Buttons/SortButton.vue";
 import { Autoplay, Pagination } from "swiper/modules";
 import stadiumList from "@/assets/data/stadiumList.json";
 import useMoneyFormatter from "../../mixins/currencyFormatter";
@@ -15,31 +16,15 @@ const modules = [Autoplay, Pagination];
 
 const slidesPerView = computed(() => {
   if (screenSize.value < 340) {
-    return 1.3;
-  } else if (screenSize.value < 377) {
-    return 1.5;
-  } else if (screenSize.value < 400) {
-    return 1.6;
-  } else if (screenSize.value < 440) {
-    return 1.7;
+    return 1;
   } else if (screenSize.value < 464) {
-    return 1.8;
+    return 1.5;
   } else if (screenSize.value < 510) {
-    return 2;
-  } else {
+    return 2.5;
+  } else if (screenSize.value < 910) {
     return 3;
-  }
-});
-
-const spaceBetween = computed(() => {
-  if (screenSize.value < 360) {
-    return 12;
-  } else if (screenSize.value < 415) {
-    return -10;
-  } else if (screenSize.value < 450) {
-    return -27;
-  } else if (screenSize.value < 568) {
-    return -7;
+  } else {
+    return 4;
   }
 });
 
@@ -58,10 +43,11 @@ onMounted(() => {
   onScreenResize();
 });
 
-const currentStadion = ref("all");
+const titleList = ref(["barchasi", "band", "band emas", "narxi"]);
+const currentStadion = ref("barchasi");
 const reportStadion = computed(() => {
-  if (currentStadion.value == "all") return stadiumList;
-  if (currentStadion.value == "money")
+  if (currentStadion.value == "barchasi") return stadiumList;
+  if (currentStadion.value == "narxi")
     return stadiumList.sort((a, b) => b.price - a.price);
   return stadiumList.filter(
     (stadion) => stadion.status == currentStadion.value
@@ -69,55 +55,43 @@ const reportStadion = computed(() => {
 });
 </script>
 <template>
-  <div class="relative mx-auto max-w-3xl mb-8">
+  <div class="relative mx-auto mb-8 px-2">
     <div
-      class="flex justify-between items-center md:mx-auto text-teal-600 font-bold px-2"
+      class="flex justify-between items-center md:mx-auto text-teal-600 font-bold"
     >
       <h1 class="text-xl">Stadionlar ro'yhati</h1>
       <router-link to="/" class="text-xs">Barchasi</router-link>
     </div>
-    <ul class="flex my-2 gap-x-2">
-      <li>
-        <button
-          class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
-          :class="{ 'bg-teal-500 text-white': currentStadion == 'all' }"
-          @click="currentStadion = 'all'"
-        >
-          Barchasi
-        </button>
-      </li>
-      <li>
-        <button
-          class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
-          :class="{ 'bg-teal-500 text-white': currentStadion == 'occupied' }"
-          @click="currentStadion = 'occupied'"
-        >
-          Band
-        </button>
-      </li>
-      <li>
-        <button
-          class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
-          :class="{ 'bg-teal-500 text-white': currentStadion == 'unoccupied' }"
-          @click="currentStadion = 'unoccupied'"
-        >
-          Band bo'lmagan
-        </button>
-      </li>
-      <li class="ml-auto">
-        <button
-          class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
-          :class="{ 'bg-teal-500 text-white': currentStadion == 'money' }"
-          @click="currentStadion = 'money'"
-        >
-          Narxi bo'yicha
-        </button>
+    <ul class="hidden my-2 gap-x-2 min-[374px]:flex">
+      <li
+        v-for="(item, index) in titleList"
+        :key="index"
+        :class="{ 'ml-auto': item == 'narxi' }"
+      >
+        <SortButton
+          :class="{ 'bg-teal-500 text-white': currentStadion == item }"
+          :title="item"
+          @click="currentStadion = item"
+        />
       </li>
     </ul>
-    <!-- :centeredSlides="true" -->
+    <select
+      v-model="currentStadion"
+      class="bg-gray-50 my-2 border-teal-500 rounded-lg focus:ring-teal-500 focus:border-teal-500 text-teal-700 hidden max-[373px]:inline"
+    >
+      <option
+        v-for="(item, index) in titleList"
+        :key="index"
+        :value="item"
+        class="capitalize"
+      >
+        {{ item }}
+      </option>
+    </select>
     <swiper
+      :centeredSlides="true"
       :slidesPerView="slidesPerView"
-      :spaceBetween="spaceBetween"
+      :spaceBetween="5"
       :grabCursor="true"
       :pagination="{ clickable: true }"
       :autoplay="{
@@ -134,52 +108,49 @@ const reportStadion = computed(() => {
         @click="router.push(`/view-stadiums/${stadion.id}`)"
       >
         <div
-          class="overflow-hidden flex transition-transform duration-500 ease-in-out rounded-xl"
+          class="border w-full transition duration-300 flex flex-col rounded-xl"
         >
-          <div
-            class="border shadow-xl hover:shadow-xl w-56 transition duration-300 flex flex-col rounded-xl"
-          >
-            <div class="relative">
-              <div
-                class="w-20 h-8 flex items-center absolute top-3 left-3 bg-gray-200 text-black text-lg rounded-3xl p-2"
-              >
-                <StarIcon />
-                <a class="text-lg font-semibold ml-2">{{
-                  stadion.assessment
-                }}</a>
-              </div>
-              <div
-                class="h-36 bg-cover bg-center rounded-t-lg"
-                :style="{
-                  backgroundImage: `url(/images/${stadion.images[0]}.jpg)`,
-                }"
-              ></div>
+          <div class="relative">
+            <div
+              class="w-20 h-8 gap-x-2 flex items-center absolute top-3 left-3 bg-gray-200 text-black text-lg rounded-3xl p-2"
+            >
+              <StarIcon />
+              <p class="text-lg font-semibold">{{ stadion.assessment }}</p>
             </div>
-            <div class="p-2">
-              <div class="border-b-2 pb-1">
-                <h3 class="font-semibold text-zinc-900">
-                  {{ stadion.title }}
-                </h3>
-                <div
-                  class="flex justify-between text-zinc-500 text-sm font-normal"
-                >
-                  <p>{{ stadion.address }}</p>
-                  <p>{{ useMoneyFormatter(stadion.price) }}</p>
-                </div>
+            <div
+              class="h-36 bg-cover bg-center rounded-t-lg lg:h-40 xl:h-48 2xl:h-52"
+              :style="{
+                backgroundImage: `url(/images/${stadion.images[0]}.jpg)`,
+              }"
+            ></div>
+          </div>
+          <div class="p-2">
+            <div class="border-b-2 pb-1">
+              <h3 class="font-semibold text-zinc-900 md:text-xl text-lg">
+                {{ stadion.title }}
+              </h3>
+              <div
+                class="flex justify-between text-zinc-500 text-sm font-normal gap-x-2"
+              >
+                <p>{{ stadion.address }}</p>
+                <p>{{ useMoneyFormatter(stadion.price) }}</p>
               </div>
-              <div class="flex justify-between items-center mt-2">
-                <button
-                  class="bg-teal-400 hover:bg-teal-500 transition duration-200 text-sm text-gray-50 rounded-full py-1.5 px-4"
-                >
-                  Booking
-                </button>
-                <router-link
-                  :to="`/view-stadiums/${stadion.id}`"
-                  class="font-normal border-l-2 pl-5 text-zinc-500 text-lg hover:text-teal-400"
-                >
-                  Batafsil...
-                </router-link>
-              </div>
+            </div>
+            <div class="flex justify-between items-center mt-2 relative">
+              <button
+                class="bg-teal-400 hover:bg-teal-500 transition duration-200 text-sm text-gray-50 rounded-full py-1 md:py-1.5 px-3 md:px-5"
+              >
+                Booking
+              </button>
+              <div
+                class="absolute left-1/2 ml-2 bg-zinc-400 top-0 border-l h-8 w-0.5"
+              ></div>
+              <router-link
+                :to="`/view-stadiums/${stadion.id}`"
+                class="font-normal text-zinc-500 text-sm md:text-lg hover:text-teal-400"
+              >
+                Batafsil...
+              </router-link>
             </div>
           </div>
         </div>
@@ -188,7 +159,7 @@ const reportStadion = computed(() => {
   </div>
 </template>
 
-<style scoped>
+<style>
 .swiper-slide {
   min-width: fit-content;
 }
