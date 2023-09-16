dcf533777d0909a5c0be9deb848e3b1dce4de64d
Umid2720 <umidbekruzmetov309@gmail.com>
2023-09-16 17:26:51 +0500

    cho (BestPlayer): fixed background-color, edited styles

--- src/views/BestPlayers.vue
+++ src/views/BestPlayers.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { computed, ref } from "vue"
-import PlaceIcon from "@/assets/icons/PlaceIcon.vue"
+import { computed, ref } from "vue";
+import PlaceIcon from "@/assets/icons/PlaceIcon.vue";
 import bestPlayers from "@/assets/data/playerLists.json";
-import PlaceOneIcon from "@/assets/icons/PlaceOneIcon.vue"
-import PlaceTwoIcon from "@/assets/icons/PlaceTwoIcon.vue"
-import PlaceThreeIcon from "@/assets/icons/PlaceThreeIcon.vue"
+import PlaceOneIcon from "@/assets/icons/PlaceOneIcon.vue";
+import PlaceTwoIcon from "@/assets/icons/PlaceTwoIcon.vue";
+import PlaceThreeIcon from "@/assets/icons/PlaceThreeIcon.vue";
 
 const best = ref("daily");
 const reportBestPlayer = computed(() =>
@@ -14,10 +14,10 @@ const reportBestPlayer = computed(() =>
 
 <template>
   <section
-    class="px-2 overflow-y-auto max-w-lg md:mx-auto my-2"
+    class="overflow-y-auto max-w-lg md:mx-auto my-2"
     :class="{ 'max-h-96': $route.href == '/' }"
   >
-    <div class="sticky top-0 bg-gray-50 pb-4">
+    <div class="sticky top-0 pb-4 px-2 bg-white">
       <h2
         :class="[
           $route.href == '/' ? 'text-xl' : 'text-3xl my-4',
@@ -29,27 +29,27 @@ const reportBestPlayer = computed(() =>
       <ul class="flex mt-2 gap-x-2">
         <li>
           <button
-            class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
-            :class="{ 'bg-teal-500 text-white': best == 'daily' }"
             @click="best = 'daily'"
+            :class="{ 'bg-teal-500 text-white': best == 'daily' }"
+            class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
           >
             Kunlik
           </button>
         </li>
         <li>
           <button
-            class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
-            :class="{ 'bg-teal-500 text-white': best == 'weekly' }"
             @click="best = 'weekly'"
+            :class="{ 'bg-teal-500 text-white': best == 'weekly' }"
+            class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
           >
             Haftalik
           </button>
         </li>
         <li>
           <button
-            class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
-            :class="{ 'bg-teal-500 text-white': best == 'monthly' }"
             @click="best = 'monthly'"
+            :class="{ 'bg-teal-500 text-white': best == 'monthly' }"
+            class="text-sm py-1 px-4 bg-gray-200 rounded font-semibold duration-200"
           >
             Oylik
           </button>
@@ -59,12 +59,12 @@ const reportBestPlayer = computed(() =>
     <ul class="space-y-2">
       <li
         v-for="(player, index) in reportBestPlayer"
-        :key="index"
-        class="bg-white rounded-md p-2"
+        :key="player.id"
+        class="bg-white rounded-md py-2 pr-2"
       >
         <router-link
           :to="`/view-player/${player.firstName}/${player.id}`"
-          class="flex items-center justify-between"
+          class="flex items-center justify-between text-sm"
         >
           <button class="flex items-center">
             <PlaceOneIcon class="w-10 h-10" v-show="index == 0" />
